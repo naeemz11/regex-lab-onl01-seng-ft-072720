d9525065c8e728f820ae7f91374bfd66e9a5d640
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-12 15:27:49 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
 def starts_with_a_vowel?(word)
 
+ if word.scan(/^[aeiou]|^[AEIOU]/).empty?
+   false
+ else 
+ true 
 end
+end 
+
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+word= text.split(" ") 
+word.grep(/^un\w+ing$/)
 end
 
-def words_five_letters_long(text)
 
+def words_five_letters_long(text)
+word= text.split(" ")
+word.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.scan(/^[A-Z][\s\S]+\.$/).empty?
+    false
+  else
+    true
+  end
 end
 
 def valid_phone_number?(phone)
-
+if phone.scan(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/).empty?
+  false
+else
+  true
+end 
 end
